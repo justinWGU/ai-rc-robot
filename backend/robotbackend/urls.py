ebf3b9730020db8b62bd8b18353d18463e8b4cc0
justinWGU <justin_ortiz123@yahoo.com>
2025-07-18 23:41:04 -0700

    Adds url to blink led

--- backend/robotbackend/urls.py
+++ backend/robotbackend/urls.py
@@ -1,7 +1,8 @@
-from .views import rotate_camera_view, test_view
+from .views import rotate_camera_view, test_view, blink_led_view
 from django.urls import path
 
 urlpatterns = [
   path('test/', test_view),
   path('rotate-camera/', rotate_camera_view),
+  path('blink-led/', blink_led_view)
 ]
